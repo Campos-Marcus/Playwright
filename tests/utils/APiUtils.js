a49f1ec283f46af1448b158b473973e9d2cc85dd
Marcus Campos <campos.marcusrodrigues@gmail.com>
2025-11-23 16:27:34 +0000

    visual testing + screenshots strategy

--- tests/utils/APiUtils.js
+++ tests/utils/APiUtils.js
@@ -1,37 +1,28 @@
-class APiUtils
-{
-    
-    constructor(apiContext,loginPayload)
-    {
+class APiUtils{
+
+    constructor(apiContext,loginPayload){
         this.apiContext =apiContext;
         this.loginPayload = loginPayload;
     }
-    async getToken()
-    {
-        const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-            {
-                 data:this.loginPayload
-            })
- 
-                 
-                 const loginResponseJson=await loginResponse.json();
-                const token = await loginResponseJson.token;
-                 console.log(token);
-                 return token;
+
+    async getToken(){
+        const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",{data:this.loginPayload})
+        const loginResponseJson = await loginResponse.json();
+        const token = await loginResponseJson.token;
+        console.log(token);
+        return token;
     }
  
-    async createOrder(orderPayLoad)
-    {
+    async createOrder(orderPayLoad){
         let response ={};
         response.token= await this.getToken();
-        const orderResponse= await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-            {
-            data : orderPayLoad,
+        const orderResponse= await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",{ data : orderPayLoad,
             headers: {
                 'Authorization': response.token,
                 'Content-Type': 'application/json'
                  },
             })
+
             const orderResponseJson = await orderResponse.json();
             console.log(orderResponseJson);
             const orderId = orderResponseJson.orders[0];
